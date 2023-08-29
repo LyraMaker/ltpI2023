9cae411e156dba6c06a59c9224d00a9d0a4e0085
LyraMaker <gabriel.lyrat@gmail.com>
2023-08-29 08:29:35 -0300

    Atividade do quadrado em formulário

---
+++ atividades/respostas como formulário/pag13/atividade 4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Área do quadrado</title>
+</head>
+<body>
+    <form action="quadrado.php" method="post">
+        <label for="lado">Digite o lado do quadrado: </label>
+        <input type="text" name="lado" id="lado" placeholder="Digite aqui">
+        <input type="submit" value="Calcular !">
+    </form>
+</body>
+</html>
---
+++ atividades/respostas como formulário/pag13/atividade 4/quadrado.php
@@ -0,0 +1,7 @@
+<?php
+$lado = $_POST['lado'];
+
+$area = $lado**2;
+
+echo "<p><strong>O valor da área é de:</strong> $area";
+?>

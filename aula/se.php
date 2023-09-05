5cd48b04aae8392f2fe34bb901d67a1142430c31
LyraMaker <gabriel.lyrat@gmail.com>
2023-09-05 10:55:08 -0300

    Aula de condição.

---
+++ aula/se.php
@@ -0,0 +1,5 @@
+<?php
+$a = 3; $b = 5;
+
+if ($a >$b) {echo "A é maior que B";}
+else {echo "B é maior que A";}
---
+++ aula/ternario.php
@@ -0,0 +1,6 @@
+<?php
+
+$numero = 15;
+$res = $numero%2 == 0 ? "Esse número é par!": "Esse número é ímpar!";
+
+echo $res;
